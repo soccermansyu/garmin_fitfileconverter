99e5a1f3c15e94e6f8f343199aeb28be734aae78
soccermansyu <118705210+soccermansyu@users.noreply.github.com>
2023-04-30 06:18:05 +0900

    Update fitfileconverter.py

--- fitfileconverter.py
+++ fitfileconverter.py
@@ -17,13 +17,14 @@ import streamlit as st
 current_dir = os.path.dirname(__file__)
 
 # URLの制限
-if "browser.serverAddress" in st._session_state:
+if hasattr(st, "_session_state") and "browser.serverAddress" in st._session_state:
     app_url = st._session_state["browser.serverAddress"]
     if app_url != "https://shuichi-running.com/garmin-fitfileconverter/":
         st.warning("このアプリにアクセスするには、URL：https://shuichi-running.com/garmin-fitfileconverter/\nからしかアクセスできません")
         st.stop()
 else:
     st.experimental_rerun()
+
 
 
 def load_fit_tmp(path):
